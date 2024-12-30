310a392df442bfc8e3ec02f76c87bf1cdeccf443
DucHuy74 <duchuyhl74205@gmail.com>
2024-12-30 22:31:04 +0700

    add project UDPT_Server

---
+++ AddressModule/src/main/java/com/project/addressmodule/AddressModuleApplication.java
@@ -0,0 +1,13 @@
+package com.project.addressmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressModuleApplication.class, args);
+	}
+
+}
---
+++ OrderModule/src/test/java/com/project/ordermodule/OrderModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ordermodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
